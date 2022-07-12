366e0839135e1e9f29848018513b725035d7c8fc
Gustavo Selemem <gustavoselemem@hotmail.com>
2022-07-12 12:28:49 +0100

    Game website
    Website developed for a game.

---
+++ game-website/js/Items.js
@@ -0,0 +1,12 @@
+let items = {
+  pickaxe: {frame:162, mats:['wood','wood','stone']},
+  shovel : {frame:163, mats:['wood','stone','stone']},
+  wood : {frame:272},
+  stone : {frame:273},
+  berries : {frame:228, mats:['wood','wood','wood']},
+  meat : {frame:241},
+  fur : {frame: 280},
+  health_potion: {frame: 144},
+  wand_fire: {frame: 105, mats:['stone']},
+};
+export default items;
